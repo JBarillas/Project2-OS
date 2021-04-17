46c5e6cff9148620f2a17b6bdf8cbcfe58a1cfc0
AlexanderPalencia <mapg23@outlook.com>
2021-04-16 18:57:32 -0600

    adding main function

--- matmul.py
+++ matmul.py
@@ -14,3 +14,20 @@ def mulsum(array1, array2, ncolumnsB, pos):
         finalMatrix.append(np.dot(row,colum))
     return (pos, finalMatrix)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fileA", help='Name of the CSV file for Matrix A')
+    parser.add_argument("fileB", help='Name of the CSV file for Matrix B')
+    parser.add_argument("poolSize", type=int, help='Number of threads in the pool')
+    parser.add_argument("fileC", help='Name of the CSV file for the resulting Matix')
+
+    args = parser.parse_args()
+    # Capturando los argumentos
+    matrixA = str(args.fileA)
+    matrixB = str(args.fileB)
+    matrixC = str(args.fileC)
+    nthreads = args.poolSize
+
+    dfMatrixA = pd.read_csv(matrixA, header=None)
+    dfMatrixB = pd.read_csv(matrixB,  header=None)
+
